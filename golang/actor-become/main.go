a03e8eeb53e7d7a55b708b2500623aa68b11759f
Roger Johansson <roger@asynkron.se>
2023-02-07 19:30:56 +0100

    fix become example

--- golang/actor-become/main.go
+++ golang/actor-become/main.go
@@ -3,37 +3,48 @@ package main
 import (
 	"fmt"
 
+	console "github.com/asynkron/goconsole"
 	"github.com/asynkron/protoactor-go/actor"
 )
 
-type (
-	Hello            struct{ Who string }
-	SetBehaviorActor struct{}
-)
+type Hello struct {
+	Who string
+}
+type SetBehaviorActor struct {
+	actor.Behavior
+}
 
 func (state *SetBehaviorActor) Receive(context actor.Context) {
+	state.Behavior.Receive(context)
+}
+
+func (state *SetBehaviorActor) Happy(context actor.Context) {
 	switch msg := context.Message().(type) {
-	case Hello:
-		fmt.Printf("Hello %v\n", msg.Who)
-		context.SetBehavior(state.Other)
+	case *Hello:
+		fmt.Printf("Hello %v, I'm so happy\n", msg.Who)
+		state.Become(state.Angry)
 	}
 }
 
-func (state *SetBehaviorActor) Other(context actor.Context) {
+func (state *SetBehaviorActor) Angry(context actor.Context) {
 	switch msg := context.Message().(type) {
-	case Hello:
-		fmt.Printf("%v, ey we are now handling messages in another behavior", msg.Who)
+	case *Hello:
+		fmt.Printf("%v, Now I am angry!\n", msg.Who)
 	}
 }
 
 func NewSetBehaviorActor() actor.Actor {
-	return &SetBehaviorActor{}
+	a := &SetBehaviorActor{}
+	a.Become(a.Happy)
+	return a
 }
 
 func main() {
-	props := actor.FromProducer(NewSetBehaviorActor)
-	pid := actor.Spawn(props)
-	pid.Tell(Hello{Who: "Roger"})
-	pid.Tell(Hello{Who: "Roger"})
-	console.ReadLine()
+	system := actor.NewActorSystem()
+	props := actor.PropsFromProducer(NewSetBehaviorActor)
+
+	pid := system.Root.Spawn(props)
+	system.Root.Send(pid, &Hello{Who: "Roger"})
+	system.Root.Send(pid, &Hello{Who: "Roger"})
+	_, _ = console.ReadLine()
 }
